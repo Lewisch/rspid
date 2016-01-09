6b0ece0a12549f359899702fcbc8edf42d0a0b1a
Jinwook Lee <jinwook.lee365@gmail.com>
2016-01-08 18:52:59 -0800

    Printing results

--- subredditlist/spiders/baseSpider.py
+++ subredditlist/spiders/baseSpider.py
@@ -2,32 +2,34 @@ import sys
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from scrapy.selector import Selector
-from items import redditItems
+from subredditlist.items import redditItems
 from scrapy.spiders import CrawlSpider
 
+
 class baseSpider(CrawlSpider):
-	name="redditSpider"
-        def __init__(self, allowed_domains=[], start_urls=[]):
-            self.allowed_domains = allowed_domains
-            self.start_urls = start_urls
-	print "Crawling...";
+    name = "redditSpider"
 
-	def parse(self, response):
-		print "Processing data..."
-		redditPosts = Selector(response).xpath('//div[@id="siteTable"]')
-		items = []
-		for post in redditPosts:
-			item = redditItems()
-			item['postTitle'] = post.xpath('div/div[2]/p[1]/a/text()').extract()
-			item['postLink'] = post.xpath('div/div[2]/p[1]/a/@href').extract()
-			item['postUpvote'] = post.xpath('div/div[1]/div[3]/text()').extract()
-			item['commentLink'] = post.xpath('div/div[2]//ul/li[1]/a/@href').extract()
-			item['rankingPosition'] = str(0)
-			item['lastUpdate'] = post.xpath('div/div[2]/p[2]/time/@datetime').extract()
-			item['postOrigin'] = post.xpath('div/div[2]/p[2]/a/text()').extract()
-			items.append(item)
+    def __init__(self, allowed_domains=[], start_urls=[]):
+        self.allowed_domains = allowed_domains
+        self.start_urls = start_urls
+        print "Crawling..."
+        print self.allowed_domains
+        print self.start_urls
 
-		return items		
-
-
-		
+    def parse(self, response):
+        print "Processing data..."
+        print response
+        redditPosts = Selector(response).xpath('//div[@id="siteTable"]')
+        items = []
+        for post in redditPosts:
+            item = redditItems()
+            item['postTitle'] = post.xpath('div/div[2]/p[1]/a/text()').extract()
+            item['postLink'] = post.xpath('div/div[2]/p[1]/a/@href').extract()
+            item['postUpvote'] = post.xpath('div/div[1]/div[3]/text()').extract()
+            item['commentLink'] = post.xpath('div/div[2]//ul/li[1]/a/@href').extract()
+            item['rankingPosition'] = str(0)
+            item['lastUpdate'] = post.xpath('div/div[2]/p[2]/time/@datetime').extract()
+            item['postOrigin'] = post.xpath('div/div[2]/p[2]/a/text()').extract()
+            items.append(item)
+        print items
+        return items
--- subredditlist/spidgen.py
+++ subredditlist/spidgen.py
@@ -3,7 +3,8 @@ from twisted.internet import reactor
 from scrapy.crawler import CrawlerProcess
 from spiders.baseSpider import baseSpider
 
-#Use this script to create a usable spider from the baseSpider class, can add methods to use any lists of urls you want to start from
+# Use this script to create a usable spider from the baseSpider class
+# can add methods to use any lists of urls you want to start from
 
 process = CrawlerProcess()
 spide = baseSpider(['reddit.com'], ['https://www.reddit.com/r/gaming'])
